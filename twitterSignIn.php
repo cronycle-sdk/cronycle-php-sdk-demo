04b85be8f672ace5dc027101151a6c53b3a5cae3
Yuri <yuri.karkhachev@gmail.com>
2017-01-12 15:50:21 +0300

    Old twitter sign in handler replaced with unified.

--- twitterSignIn.php
+++ twitterSignIn.php
@@ -5,7 +5,7 @@ require_once 'vendor/autoload.php';
 try
 {
 	$API = new \Cronycle\Api();
-	$API->logInWithTwitter( 'http://phpsdk-demo.dev/twitterSignInHandler.php?provider=twitter2' );
+	$API->logInWithTwitter( 'http://phpsdk-demo.dev/signInHandler.php?provider=twitter2' );
 }
 catch ( \Exception $e )
 {
