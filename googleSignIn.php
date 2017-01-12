148621a93fae7878e56981c021c9bdc5a9eb3095
Yuri <yuri.karkhachev@gmail.com>
2017-01-12 15:49:19 +0300

    Removed separate handlers for social sign in and created unified social sign in handler. Provider parameter should be passed for the callback when init sign in.

--- googleSignIn.php
+++ googleSignIn.php
@@ -5,7 +5,7 @@ require_once 'vendor/autoload.php';
 try
 {
 	$API = new \Cronycle\Api();
-	$API->logInWithGoogle( 'http://phpsdk-demo.dev/googleSignInHandler.php' );
+	$API->logInWithGoogle( 'http://phpsdk-demo.dev/signInHandler.php?provider=google_oauth2' );
 }
 catch ( \Exception $e )
 {
